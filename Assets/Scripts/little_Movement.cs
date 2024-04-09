9c84382c1006ee835351a5021c6eed64e632b189
Hystabec <edward14577777@gmail.com>
2024-04-09 13:48:33 +0100

    finished movement

--- Assets/Scripts/little_Movement.cs
+++ Assets/Scripts/little_Movement.cs
@@ -12,18 +12,17 @@ public class little_Movement : MonoBehaviour
     public float jumpForce = 100.0f;
 
     LayerMask groundMask;
-    
 
     bool isGrounded()
     {
         bool returnbool = false;
 
-        RaycastHit2D rayHit = Physics2D.Raycast(transform.position, Vector2.down, distanceToGround, groundMask);
+        //RaycastHit2D rayHit = Physics2D.Raycast(transform.position, Vector2.down, distanceToGround, groundMask);
+        RaycastHit2D rayHitleft = Physics2D.Raycast(transform.position + new Vector3(0.2f, 0, 0), Vector2.down, distanceToGround, groundMask);
+        RaycastHit2D rayHitright = Physics2D.Raycast(transform.position + new Vector3(-0.2f, 0, 0), Vector2.down, distanceToGround, groundMask);
 
-        if (rayHit.collider != null)
+        if ((rayHitleft.collider != null) || (rayHitright.collider != null))
             returnbool = true;
-
-        Debug.Log("grounded: " + returnbool);
         return returnbool;
     }
 
@@ -38,7 +37,6 @@ public class little_Movement : MonoBehaviour
     {
         if(isGrounded() && Input.GetKey(KeyCode.UpArrow))
         {
-            Debug.Log("jump");
             rb.AddForce(new Vector2(0, jumpForce*10));
         }
         else if (!isGrounded())
@@ -49,14 +47,12 @@ public class little_Movement : MonoBehaviour
         //move left
         if(Input.GetKey(KeyCode.LeftArrow))
         {
-            Debug.Log("left");
             gameObject.transform.position -= new Vector3(moveSpeed, 0, 0);
         }
 
         //move right
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            Debug.Log("right");
             gameObject.transform.position -= new Vector3(-moveSpeed, 0, 0);
         }
     }
