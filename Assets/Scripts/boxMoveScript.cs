61360998d64fb654f4dff2110a47cb597641d5f9
LemonBoi0211 <lemonboi0211@outlook.com>
2024-04-09 15:03:27 +0100

    fixed coop prototype

--- Assets/Scripts/boxMoveScript.cs
+++ Assets/Scripts/boxMoveScript.cs
@@ -4,12 +4,46 @@ using UnityEngine;
 
 public class boxMoveScript : MonoBehaviour
 {
+    bool lockedPos = false;
+
+    Rigidbody2D rb;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag == "Player1")
         {
             Vector3 direct = transform.position - collision.gameObject.transform.position;
             gameObject.GetComponent<Rigidbody2D>().AddForce(-direct);
+        }
+        else if(collision.gameObject.tag == "Player2")
+        {
+            lockedPos = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player2")
+        {
+            lockedPos = false;
+        }
+    }
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>(); 
+    }
+
+    private void Update()
+    {
+        if (lockedPos)
+        {
+            rb.constraints = RigidbodyConstraints2D.FreezePosition;
+        }
+        else
+        {
+            rb.constraints = RigidbodyConstraints2D.None;
+            rb.constraints = RigidbodyConstraints2D.FreezeRotation;
         }
     }
 }
